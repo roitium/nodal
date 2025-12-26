2fb8d74939219f47622bdd679858243ec1d5fca3
Roitium. <65794453+roitium@users.noreply.github.com>
2025-12-27 07:38:41 +0800

    chore: build.gradle.kts

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 android {
     namespace = "com.roitium.nodal"
     
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.roitium.nodal"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
